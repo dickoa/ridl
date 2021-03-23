fb48ac36a5dfce43467bb50d66e1f7b491636454
Ahmadou Dicko <mail@ahmadoudicko.com>
2021-03-23 17:28:52 +0000

    Add new fixtures

--- tests/testthat/setup-ridl.R
+++ tests/testthat/setup-ridl.R
@@ -1,6 +1,21 @@
 library("vcr")
+
+vcr_dir <- "../fixtures"
+
+if (!nzchar(Sys.getenv("RIDL_API_KEY"))) {
+  if (dir.exists(vcr_dir)) {
+    # Fake API token to fool our package
+    Sys.setenv("RIDL_API_KEY" = "foobar")
+  } else {
+    # If there's no mock files nor API token, impossible to run tests
+    stop("No API key nor cassettes, tests cannot be run.",
+         call. = FALSE)
+  }
+}
+
 invisible(vcr::vcr_configure(
-  filter_sensitive_data = list("<<<my_api_key>>>" = Sys.getenv('RIDL_API_KEY')),  # add this
-  dir = "../fixtures"
+  dir = vcr_dir,
+  preserve_exact_body_bytes = TRUE,
+  filter_request_headers = list(Authorization = "My bearer token is safe")
 ))
 vcr::check_cassette_names()
